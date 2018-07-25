20aa0a98aeb7a6a9201e2640a9346b1db085349b
André Drougge <andre.drougge@gmail.com>
2018-07-25 12:42:17 +0200

    Add new libs to default exports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 export * from './src/Form.utils';
 export * from './src/Model.utils';
-// export * from './src/Static.utils';
-// export * from './src/Storage.utils';
+export * from './src/Image.utils';
+export * from './src/API.utils';
+export * from './src/CRUD';
