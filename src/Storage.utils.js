2bdff695282fb27137599a405ffc0feea93d7f9c
André Drougge <andre.drougge@gmail.com>
2019-02-04 15:42:51 +0200

    Add utility class for cookies. SWEEPR-1219

---
+++ src/Storage.utils.js
@@ -0,0 +1,29 @@
+const { host } = window.location;
+
+/** CookieStorage
+ * @description Utility library for using cookies both in development and prod.
+ */
+export class CookieStorage {
+  /**
+   * setup
+   * @description Prepares given cookies library for usage.
+   * - Sets default path
+   * - Sets default domain
+   */
+  static setup(cookies) {
+    try {
+      cookies.defaults = { path: '/', domain: this.domain };
+    } catch (e) {
+      console.warn('Cookie setup error', e);
+    }
+  }
+
+  /**
+   * domain
+   * @returns {string} Returns current domain, usable when managing cookies.
+   */
+  static get domain() {
+    if (/localhost/g.test(host)) return 'localhost';
+    return host;
+  }
+}
