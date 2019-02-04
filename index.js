4114b090c721e15f0a7fd6ae8db2aed7b3852254
André Drougge <andre.drougge@gmail.com>
2019-02-04 15:42:51 +0200

    Add utility class for cookies. SWEEPR-1219

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-
 export * from './src/Form.utils';
 export * from './src/Model.utils';
 export * from './src/Image.utils';
 export * from './src/API.utils';
+export * from './src/Storage.utils';
 export * from './src/CRUD';
