5fa42311f9494e52f9d062684150cf2b7efc7469
André Drougge <andre.drougge@gmail.com>
2019-10-28 10:12:15 +0200

    Add test suites for parameterize and deParameterize

---
+++ test/deParameterize.spec.js
@@ -0,0 +1,27 @@
+import sinon from "sinon";
+import { expect } from "chai";
+import _ from "lodash";
+import deParameterize from "../src/deParameterize";
+
+describe("DeParameterize", () => {
+  it("should exist", () => {
+    expect(deParameterize).to.exist;
+  });
+
+  it("should return an object", () => {
+    expect(deParameterize("?first=1st&second=2nd")).to.be.an("object");
+  });
+
+  it("should return expected object", () => {
+    expect(deParameterize("?first=1st&second=2nd")).to.eql({
+      first: "1st",
+      second: "2nd"
+    });
+  });
+
+  it("should leave leading question mark optional", () => {
+    expect(deParameterize("?third=3rd&fourth=4th&first=1st&second=2nd")).to.eql(
+      { third: "3rd", fourth: "4th", first: "1st", second: "2nd" }
+    );
+  });
+});
