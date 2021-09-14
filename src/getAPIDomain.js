c26447346e09e045d00f3b5dc41295aa5cb6728a
André Drougge <andre.drougge@gmail.com>
2021-09-14 08:57:48 +0300

    Add getAPIDomain util fn to library

---
+++ src/getAPIDomain.js
@@ -0,0 +1,32 @@
+/**
+ * getAPIDomain
+ * @since 1.7.3
+ * @category URL
+ * @description Looks at the host and discards the first part and replaces it
+ * with a given prefix. Special case for localhost.
+ * @param {String} host
+ * @param {String} prefix
+ * @param {String} dev 'qa', 'mobile', 'dev' or any other environment.
+ * @returns {string} Returns new string.
+ * @example
+ * getAPIDomain('admin.sweepr.com', 'api');
+ * #=> 'api.sweepr.com'
+ *
+ * getAPIDomain('some-new.sub-domain.example.co.uk', 'cms');
+ * #=> 'cms.sub-domain.example.co.uk'
+ *
+ * getAPIDomain('admin.dev-provider.sweepr.com', 'dev-api');
+ * #=> 'dev-api.dev-provider.sweepr.com'
+ *
+ * getAPIDomain('localhost:3000', 'api', 'qa');
+ * #=> 'api.qa.sweepr.com'
+ */
+const getAPIDomain = (host, prefix, dev = "mobile") => {
+  if (/localhost/g.test(host)) return `${prefix}.${dev}.sweepr.com`;
+  let h = host.split(".");
+  h.splice(0, 1);
+  h = h.join(".");
+  return `${prefix}.${h}`;
+};
+
+export default getAPIDomain;
